2f9c3fd6d64d4b17b09252539e3b44f9a72f8adb
Ace Baugh <ace.d.baugh@gmail.com>
2026-03-13 23:24:53 -0400

    New code

--- node_modules/gurgi-client/src/types/index.ts
+++ node_modules/gurgi-client/src/types/index.ts
@@ -1,37 +1,44 @@
 export interface Location {
- _id: string;
- name: string;
- slug: string;
+  _id: string;
+  name: string;
+  slug: string;
 }
 
 export interface Ride {
- _id: string;
- name: string;
- nameSlug: string;
- location: Location;
- rideType: string;
- guests: number[][] | number[];
- evenOddLines: boolean;
- singleRiders: boolean;
- rowRequest: boolean;
- doubleGroupable: boolean;
- active: boolean;
+  _id: string;
+  name: string;
+  nameSlug: string;
+  location: Location;
+  rideType: string;
+  guests: number[][] | number[];
+  evenOddLines: boolean;
+  singleRiders: boolean;
+  rowRequest: boolean;
+  doubleGroupable: boolean;
+  active: boolean;
+}
+
+export interface Guest {
+  id: string;
+  groupId: string;
+  color: string;
+  selected: boolean;
 }
 
 export interface GuestGroup {
- id: string;
- size: number;
- isActive: boolean;
- color?: string;
- selected?: boolean;
+  id: string;
+  size: number;
+  isActive: boolean;
+  color?: string;
+  selected?: boolean;
 }
 
 export interface GameConfig {
- vehiclesToComplete: number;
- visibleGuests: number;
- maxGroupSize: number;
- timerEnabled: boolean;
- timerDuration: number;
- tapToShow: boolean;
- doubleGrouping: boolean;
+  vehiclesToComplete: number;
+  visibleGuests: number;
+  maxGroupSize: number;
+  timerEnabled: boolean;
+  timerDuration: number;
+  tapToShow: boolean;
+  doubleGrouping: boolean;
 }
